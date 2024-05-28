bd427d71a925223afc0be3eccfd39a0b7de0cf9d
atikfaisalemon <atikfaisalemon@gmail.com>
2024-05-28 20:09:51 +0600

    fully worked, can crete and delete note

--- main.js
+++ main.js
@@ -1,49 +1,42 @@
 const inputText = document.getElementById("input");
 const listContaner = document.getElementById("list");
 const addButton = document.getElementById("btn");
-const inputCheckbox = document.getElementById("default-checkbox");
-
-inputCheckbox.addEventListener("change", (e) => {
-  //change label text color
-  console.log("e.target.checked", e.target.checked);
-  console.log("taskName", e);
-  if (e.target.checked) {
-    taskName.style.textDecorationLine = "line-through";
-  } else {
-    taskName.style.textDecorationLine = "none";
-  }
-});
 
 addButton.addEventListener("click", () => {
   if (inputText.value === "") {
     alert("Please enter a value");
   } else {
-    //const current Time
-    const currentTime = new Date().toISOString();
-    const listItem = document.createElement("li");
-    listItem.classList.add("task-item");
-    // listItem.id = `task-${currentTime}`;
-    const divItem = document.createElement("div");
-    divItem.classList.add("task-item__div");
-    const inputItem = document.createElement("input");
-    inputItem.type = "checkbox";
-    inputItem.classList.add("task-item__input");
-    const label = document.createElement("label");
-    // label.id = `label-${currentTime}`;
-    label.textContent = inputText.value;
+    // List Item
+    const liItem = document.createElement("li");
+    liItem.innerHTML = `<span>${inputText.value}</span>`;
+    listContaner.appendChild(liItem);
 
-    divItem.appendChild(inputItem);
-    divItem.appendChild(label);
+    // Checkbox
+    const input = document.createElement("input");
+    input.type = "checkbox";
+    liItem.prepend(input);
 
-    listItem.appendChild(divItem);
+    // Checkbox Event
+    input.addEventListener("change", () => {
+      if (input.checked) {
+        liItem.getElementsByTagName("span")[0].style.textDecoration =
+          "line-through";
+        liItem.style.backgroundColor = "gray";
+        liItem.style.color = "white";
+      } else {
+        liItem.getElementsByTagName("span")[0].style.textDecoration = "none";
+        liItem.style.backgroundColor = "white";
+        liItem.style.color = "black";
+      }
+    });
 
-    const deleteBtn = document.createElement("button");
-    deleteBtn.textContent = "Delete";
-    deleteBtn.classList.add("delete-btn");
-
-    listItem.appendChild(deleteBtn);
-
-    //finally list item send to ul
-    listContaner.appendChild(listItem);
+    // Delete Button
+    const btnn = document.createElement("button");
+    btnn.textContent = "Delete";
+    liItem.appendChild(btnn);
+    btnn.addEventListener("click", () => {
+      listContaner.removeChild(liItem);
+    });
+    inputText.value = "";
   }
 });
